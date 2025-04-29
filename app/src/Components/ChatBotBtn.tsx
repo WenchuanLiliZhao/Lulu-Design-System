aa87e4c7e288e558195e9a99d1fd6997098444ef
Wenchuan Zhao <111623150+WenchuanLiliZhao@users.noreply.github.com>
2025-04-29 23:59:34 +0800

    Update ChatBotBtn.tsx

--- app/src/Components/ChatBotBtn.tsx
+++ app/src/Components/ChatBotBtn.tsx
@@ -50,11 +50,6 @@ export const ChatBotBtn = () => {
             的样式能多大程度地自定义。如果可以的话，请给我在 Jira 里开一个
             task，并在 task description
             里大致告诉我需要我在设计图中提供一些什么。不过，这个功能的易用性基本已经到位了，因此优先级很低——但这不妨碍我们在
-            Jira 例开一个 taskundefinedーケーア，ミンゴー！这是 chatbox
-            的占位。我不确定这个 chatbox
-            的样式能多大程度地自定义。如果可以的话，请给我在 Jira 里开一个
-            task，并在 task description
-            里大致告诉我需要我在设计图中提供一些什么。不过，这个功能的易用性基本已经到位了，因此优先级很低——但这不妨碍我们在
             Jira 例开一个 task！<br />
             —— 文川·莉莉·赵
           </div>
