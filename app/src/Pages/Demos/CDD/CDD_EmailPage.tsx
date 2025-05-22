74f2d9afd447ab25f849b9822ef5a8fb7f8260bf
Wenchuan Zhao <111623150+WenchuanLiliZhao@users.noreply.github.com>
2025-05-22 12:00:51 +0800

    u

--- app/src/Pages/Demos/CDD/CDD_EmailPage.tsx
+++ app/src/Pages/Demos/CDD/CDD_EmailPage.tsx
@@ -1,6 +1,7 @@
 import { PageShape } from "../../../ObjectShapes/PageShape";
 import { ReactNode, useRef } from "react";
 import { CDD_Nav } from "./CDD_SiteConfig";
+import React from "react";
 
 interface CalloutProps {
   type: "Caution" | "Information" | "Warning" | "Danger";
@@ -35,75 +36,9 @@ export const EmailCallout: React.FC<CalloutProps> = ({ type, content }) => {
   );
 };
 
-type EmailListCardItemType = {
-  title: string;
-  content: string;
-};
-
-interface EmailListCardProps {
-  items: EmailListCardItemType[];
-}
-
-export const EmailListCard: React.FC<EmailListCardProps> = ({ items }) => {
-  const styles = {
-    container: {
-      backgroundColor: "rgba(129, 129, 129, 0.08)",
-      padding: "1rem",
-      borderRadius: "4px",
-      border: "1px solid rgba(129, 129, 129, 0.08)",
-    },
-    item: {},
-    title: {
-      fontWeight: "bold",
-      fontSize: "14px",
-      lineHeight: "1.5",
-    },
-    content: {
-      fontSize: "14px",
-      lineHeight: "1.5",
-      marginTop: "0.25rem",
-    },
-  };
-
-  return (
-    <div style={{ ...styles.container }}>
-      {items.map((item: EmailListCardItemType, index: number) => (
-        <div
-          style={{
-            ...styles.item,
-            ...(index !== 0 && {
-              marginTop: "0.5rem",
-              borderTop: "1px solid rgba(129, 129, 129, 0.08)",
-              paddingTop: "0.5rem",
-            }),
-          }}
-          key={index}
-        >
-          <div style={{ ...styles.title }}>{item.title}</div>
-          <div style={{ ...styles.content }}>{item.content}</div>
-        </div>
-      ))}
-    </div>
-  );
-};
-
 interface CDD_EmailPageProps {
   children: ReactNode;
 }
-
-export const EmailTwoColumnContainer: React.FC<{ children: ReactNode }> = ({
-  children,
-}) => {
-  const isMobile = window.innerWidth < 600;
-
-  const styles = {
-    display: "grid",
-    gridTemplateColumns: isMobile ? "1fr" : "repeat(2, 1fr)",
-    gap: "1rem",
-  };
-
-  return <div style={{ ...styles, ...emailItemStyles }}>{children}</div>;
-};
 
 export const EmailParagraph: React.FC<{ children: ReactNode }> = ({
   children,
@@ -116,8 +51,11 @@ export const EmailParagraph: React.FC<{ children: ReactNode }> = ({
 };
 
 type EmailTableHeaderType = {
-  title: string;
-  content: string[];
+  title?: string;
+  content: {
+    title?: string;
+    value: string;
+  }[];
 };
 
 interface EmailTableProps {
@@ -125,30 +63,42 @@ interface EmailTableProps {
 }
 
 export const EmailTable: React.FC<EmailTableProps> = ({ rows }) => {
+  // Find the maximum number of content items across all rows
+  const maxContentLength = Math.max(...rows.map(row => row.content.length));
+
   return (
     <div style={{ overflowX: "auto" }}>
-      <table style={{ minWidth: "800px", width: "100%", fontSize: "14px", borderCollapse: "collapse" as 'collapse' }}>
+      <table
+        style={{
+          width: "100%",
+          fontSize: "14px",
+          borderCollapse: "collapse",
+        }}
+      >
         <thead>
           <tr>
             {rows.map((row, k: number) => (
-              <th
-                key={k}
-                style={{
-                  textAlign: "left",
-                  border: "1px solid rgba(129, 129, 129, 0.16)",
-                  backgroundColor: "rgba(129, 129, 129, 0.08)",
-                  padding: "0.5rem",
-                  fontSize: "14px",
-                  lineHeight: "1.5",
-                }}
-              >
-                {row.title}
-              </th>
+              <React.Fragment key={k}>
+                {row.title && (
+                  <th
+                    style={{
+                      textAlign: "left",
+                      border: "1px solid rgba(129, 129, 129, 0.16)",
+                      backgroundColor: "rgba(129, 129, 129, 0.08)",
+                      padding: "0.5rem",
+                      fontSize: "14px",
+                      lineHeight: "1.5",
+                    }}
+                  >
+                    {row.title}
+                  </th>
+                )}
+              </React.Fragment>
             ))}
           </tr>
         </thead>
         <tbody>
-          {rows[0].content.map((_, rowIndex) => (
+          {Array.from({ length: maxContentLength }).map((_, rowIndex) => (
             <tr key={rowIndex}>
               {rows.map((row, colIndex) => (
                 <td
@@ -161,7 +111,17 @@ export const EmailTable: React.FC<EmailTableProps> = ({ rows }) => {
                     lineHeight: "1.5",
                   }}
                 >
-                  {row.content[rowIndex]}
+                  {row.content[rowIndex] && (
+                    <>
+                      {row.content[rowIndex].title && (
+                        <>
+                          <strong style={{fontWeight: "bold"}}><span>{row.content[rowIndex].title}</span></strong>
+                          <br />
+                        </>
+                      )}
+                      <span>{row.content[rowIndex].value}</span>
+                    </>
+                  )}
                 </td>
               ))}
             </tr>
@@ -235,67 +195,110 @@ export const CDD_EmailPage: PageShape = {
                 "This email originated from outside of the organization. Do not click links, scan QR codes, or open attachments unless you can confirm the sender and know the content is safe. If you think this email might be suspicious, notify lululemon's Cybersecurity team by clicking the HOX button."
               }
             />
-            <EmailTwoColumnContainer>
-              <EmailListCard
-                items={[
-                  {
-                    title: "异常数据量",
-                    content: "2（总数据量2，占比100.0%）",
-                  },
-                  { title: "规则名称", content: "TryOn设备预警" },
-                  { title: "规则描述", content: "TryOn设备预警" },
-                  {
-                    title: "检查对象",
-                    content:
-                      "门店维度信息(dim_store), fact_rfid_fitting_room_trial_log_di",
-                  },
-                  {
-                    title: "检查时间",
-                    content: "2025-05-21 08:00:05~2025-05-21 08:00:11",
-                  },
-                ]}
-              />
-              <EmailListCard
-                items={[
-                  { title: "结果描述", content: "严重" },
-                  { title: "归属专项", content: "试穿设备监控" },
-                  { title: "负责人", content: "CK Chen （陈恺）" },
-                  { title: "归属系统", content: "DataLake" },
-                  { title: "数据源", content: "DataLake_prod" },
-                ]}
-              />
-            </EmailTwoColumnContainer>
-            <EmailParagraph>{"异常数据样例如下,详情见附件："}</EmailParagraph>
             <EmailTable
               rows={[
                 {
-                  title: "warehouse_slug",
-                  content: ["98765", "54321"],
+                  content: [
+                    {
+                      title: "异常数据量",
+                      value: "2（总数据量2，占比100.0%）",
+                    },
+                    {
+                      title: "规则名称",
+                      value: "TryOn设备预警",
+                    },
+                    {
+                      title: "规则描述",
+                      value: "TryOn设备预警",
+                    },
+                    {
+                      title: "检查对象",
+                      value: "门店维度信息(acropolis), ach_fact_tryon_device_info",
+                    },
+                    {
+                      title: "检查时间",
+                      value: "2025-05-21 08:00:05~2025-05-21 08:00:11",
+                    },
+                  ],
                 },
                 {
-                  title: "store_id_description",
-                  content: ["假店名一", "假店名二"],
+                  content: [
+                    {
+                      title: "结果描述",
+                      value: "严重",
+                    },
+                    {
+                      title: "归属专项",
+                      value: "竞技场无法加载",
+                    },
+                    {
+                      title: "负责人",
+                      value: "阿尔忒弥斯",
+                    },
+                    {
+                      title: "归属系统",
+                      value: "Aropolis",
+                    },
+                    {
+                      title: "数据源",
+                      value: "Aropolis_TryOn",
+                    },
+                  ],
+                },
+              ]}
+            />
+            <EmailParagraph>{"异常数据样例如下,详情见附件："}</EmailParagraph>
+            
+            <EmailTable
+              rows={[
+                {
+                  title: "warehouse_code",
+                  content: [
+                    { value: "23451" },
+                    { value: "12345" },
+                  ],
+                },
+                {
+                  title: "store_name_cn",
+                  content: [
+                    { value: "幽暗城理发店" },
+                    { value: "吉他 IV 加达里交易中心" },
+                  ],
                 },
                 {
                   title: "status",
-                  content: ["Inactive", "Inactive"],
+                  content: [
+                    { value: "Opened" },
+                    { value: "Opened" },
+                  ],
                 },
                 {
                   title: "open_date",
-                  content: ["2022-02-02", "2022-07-20"],
+                  content: [
+                    { value: "2021-01-14" },
+                    { value: "2025-01-22" },
+                  ],
                 },
                 {
                   title: "门店设备数-前1日采集到试穿",
-                  content: ["4", "2"],
+                  content: [
+                    { value: "1" },
+                    { value: "6" },
+                  ],
                 },
                 {
                   title: "门店试衣间数-MDM",
-                  content: ["5", "3"],
+                  content: [
+                    { value: "11" },
+                    { value: "8" },
+                  ],
                 },
               ]}
             />
             <EmailParagraph>
-              <p style={{color: "royalblue"}}>更多详细信息请登录China Data Discover</p>
+              <p style={{ color: "royalblue" }}>
+                更多详细信息请登录China Data Discover
+              </p>
             </EmailParagraph>
           </>
         }
